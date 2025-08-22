62a3388cd5c9801bf6430dbd7b937b23cf74cec1
NijatZadeh <njtght@outlook.com>
2025-08-22 07:55:35 -0700

    Update ARP_SCANner.py

--- ARP_SCANner.py
+++ ARP_SCANner.py
@@ -9,25 +9,21 @@ banner = r"""
         | \IIIIII/ |
         \          /
          `--------`
-
+         
       [ ARP  SCANNER ]
 """
 
 print(banner)
 
-from scapy.all import ARP, Ether, srp 
+from scapy.all import ARP, Ether, srp
 
-target = "TARGET_IP/CIDR" #EXAMPLE: "192.168.1.1/24"
+target = "192.168.1.1/24" 
 
 arp = ARP(pdst=target) 
-ether = Ether(dst="ff:ff:ff:ff:ff:ff") #UNIVERSAL BROADCAST
+ether = Ether(dst="ff:ff:ff:ff:ff:ff") 
 
-packet = ether/arp  
+packet = ether/arp 
 result = srp(packet, timeout=2, verbose=0)[0] 
 
 for sent, received in result:
-	print(f"IP: {received.psrc} MAC: {received.hwsrc}")
-
-#==========================ArpScan===============================
-
-
+        print(f"IP: {received.psrc} MAC: {received.hwsrc}")
